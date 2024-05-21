9b865447d7df3360ff47d01415f37093c8f6789b
knilesh9 <neutron_stays@protonmail.com>
2024-05-21 23:03:47 +0530

    All section styling done

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,15 @@ const App = () => (
     <section className="padding">
       SpecialOffers
     </section>
+    <section className="bg-pale-blue padding">
+      CustomerReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      Footer
+    </section>
   </main>
 )
 
