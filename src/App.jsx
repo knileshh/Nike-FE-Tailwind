ec5884f2e7c54c65e774edb3ba600a7033878af3
knilesh9 <neutron_stays@protonmail.com>
2024-05-26 20:26:39 +0530

    Now all are organized now we just need to create the main contents using the JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
-import Hero from './sections/Hero'
-
+import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from "./sections";
 const App = () => (
   <main className="relative">
-    Nav
+    {/* Nav */}
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero/>
     </section>
     <section className="padding">
-      PopularProduct
+      <PopularProducts/>
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality/>
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services/>
     </section>
     <section className="padding">
-      SpecialOffers
+      <SpecialOffer/>
     </section>
     <section className="bg-pale-blue padding">
-      CustomerReviews
+      <CustomerReviews/>
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe/>
     </section>
     <section className="bg-black padding-x padding-t pb-8">
-      Footer
+      <Footer/>
     </section>
   </main>
 )
