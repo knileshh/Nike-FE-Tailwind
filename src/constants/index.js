cec2c80a475dda2fed4e339e7b0423e348b39260
knilesh9 <neutron_stays@protonmail.com>
2024-06-02 13:31:37 +0530

    And now we're done with practising tailwind

--- src/constants/index.js
+++ src/constants/index.js
@@ -112,7 +112,7 @@ export const footerLinks = [
         title: "Get in touch",
         links: [
             { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-            { name: "+92554862354", link: "tel:+92554862354" },
+            { name: "+92554862354", link: "tel:+91 004862354" },
         ],
     },
 ];
