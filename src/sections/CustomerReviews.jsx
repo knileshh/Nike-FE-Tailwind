054c5bdb48bde447cfdb125a046479020084000a
knilesh9 <neutron_stays@protonmail.com>
2024-06-02 11:10:15 +0530

    Done with customer reviews also

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,6 +1,27 @@
+import { reviews } from "../constants"
+import ReviewCard from "../components/ReviewCard"
+
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our 
+        <span>Customers</span>
+      </h3>
+
+      <p className="m-auto mt-4 max-w-lg  text-center info-text">Hear geneuine storeis from our customers about their exceptional experiences with us</p>
+
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-cl gap-14">
+        {reviews.map((review) => (
+          <ReviewCard key={review.customerName} imgURL={review.imgURL}
+          customerName={review.customerName}
+          rating={review.rating}
+          feedback={review.feedback}
+          />
+        ))}
+
+      </div>
+    </section>
   )
 }
 
